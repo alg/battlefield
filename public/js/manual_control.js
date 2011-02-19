5e359ac2052c1c8a5252c1302531a1f3ca7d7d54
Alexander Glushkov <cutalion@gmail.com>
2011-02-19 17:58:41 +0200

    use keydown/keyup instead of keypress

--- public/js/manual_control.js
+++ public/js/manual_control.js
@@ -1,28 +1,39 @@
 function ManualControl(app){
-  var BTN_LEFT  = 97;
-  var BTN_UP    = 119;
-  var BTN_RIGHT = 100;
-  var BTN_DOWN  = 115;
+  var BTN_LEFT  = 65;
+  var BTN_UP    = 87;
+  var BTN_RIGHT = 68;
+  var BTN_DOWN  = 83;
   var BTN_SPACE = 32;
+  this.command = { move: 0, turn: 0, fire: 0 }
 
   this.move_ahead = function(){
-    this.move = this.move < 0 ? 0 : 1;
-    app.command({move: move, turn: 0});
+    this.command.move = this.command.move < 0 ? 0 : 1;
+    app.command(this.command);
   }
 
   this.move_back = function(){
-    this.move = this.move > 0 ? 0 : -1;
-    app.command({move: this.move, turn: 0});
+    this.command.move = this.commandmove > 0 ? 0 : -1;
+    app.command(this.command);
+  }
+
+  this.stop_move = function(){
+    this.command.move = 0
+    app.command(this.command)
+  }
+
+  this.stop_turn = function(){
+    this.command.turn = 0
+    app.command(this.command)
   }
 
   this.turn_left = function(){
-    var turn = this.move < 0 ? 1 : -1;
-    app.command({turn: turn})
+    this.command.turn = this.command.move < 0 ? 1 : -1;
+    app.command(this.command)
   }
 
   this.turn_right = function(){
-    var turn = this.move >= 0 ? 1 : -1;
-    app.command({turn: turn})
+    this.command.turn = this.command.move >= 0 ? 1 : -1;
+    app.command(this.command)
   }
 
   this.fire = function(){
@@ -31,13 +42,28 @@ function ManualControl(app){
 
   var control = this;
 
-  $(document).keypress(function(event){
+  $(document).keydown(function(event){
     switch(event.which){
       case BTN_UP:    control.move_ahead(); break;
       case BTN_DOWN:  control.move_back(); break;
       case BTN_LEFT:  control.turn_left(); break;
       case BTN_RIGHT: control.turn_right(); break;
       case BTN_SPACE: control.fire(); break;
+    }
+  })
+
+
+  $(document).keyup(function(event){
+    switch(event.which){
+      case BTN_UP:    
+      case BTN_DOWN:  
+        control.stop_move();
+      break;
+
+      case BTN_LEFT:
+      case BTN_RIGHT: 
+        control.stop_turn(); 
+      break;
     }
   })
 
